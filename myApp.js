edb0803d49c71f9fedaf7cb53a9734b7f37185ec
martinvplopez <vanpu3d@gmail.com>
2021-09-15 10:07:56 +0200

    Json serving

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get("/", (req,res)=>{
     res.sendFile(path);
 });
 
+app.get("/json", (req,res)=>{
+    res.json({"message": "Hello json"});
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 
