cc88e523183063854c8076ae4ab9fc7792eb722e
martinvplopez <vanpu3d@gmail.com>
2021-09-15 11:15:41 +0200

    Chained Middleware

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use("/", (req, res, next)=>{
 });
 
 app.get("/now", (req,res,next)=>{
-    req.time=Date.now().toString();
+    req.time=new Date().toString();
     next();
 },function(req,res){
     res.send({time: req.time});
