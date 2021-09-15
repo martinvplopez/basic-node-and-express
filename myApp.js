23b862a9a3051f0a2ba91611f172ad3b2768b517
martinvplopez <vanpu3d@gmail.com>
2021-09-15 10:00:13 +0200

    Express Get Handling

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+let path= __dirname + "/views/index.html"
+
 app.get("/", (req,res)=>{
-    res.send("Hello Express");
+    res.sendFile(path);
 });
 
 
